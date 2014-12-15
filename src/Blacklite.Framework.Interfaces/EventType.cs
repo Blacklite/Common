7e3491db390dcc755c33c97a89d3a60990224c10
David Driscoll <david.driscoll@gmail.com>
2014-12-15 13:21:02 -0500

    Added event type enum

---
+++ src/Blacklite.Framework.Interfaces/EventType.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Framework.Runtime;
+using System;
+
+namespace Blacklite.Framework
+{
+    [AssemblyNeutral]
+    public enum EventType
+    {
+        ResetCache,
+        ResetMetadata,
+    }
+}
