b1fed50368572d9ef9a3e44639e054e497174af4
David Driscoll <david.driscoll@gmail.com>
2015-01-19 16:34:55 -0500

    Added missing files

---
+++ src/Blacklite.Framework.GlobalEvents/IEventObservable.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Blacklite.Framework
+{
+    public interface IEventObservable<T> : IObservable<T> where T : IEvent { }
+}
---
+++ src/Blacklite.Framework.GlobalEvents/IEventOrchestrator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Blacklite.Framework
+{
+    public interface IEventOrchestrator<T> where T : IEvent
+    {
+        void Broadcast([NotNull] T value);
+        IObservable<T> Events { get; }
+    }
+}
