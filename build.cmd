e62ed28f428f32eb9198dd0beef9566137db15d7
David Driscoll <david.driscoll@gmail.com>
2015-01-24 13:12:28 -0500

    appveyor: Update build.cmd again

--- build.cmd
+++ build.cmd
@@ -20,8 +20,8 @@ IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
 IF "%SKIP_KRE_INSTALL%"=="1" goto run
-CALL packages\KoreBuild\build\dotnetsdk upgrade -amd64
-CALL packages\KoreBuild\build\dotnetsdk install default -runtime CoreCLR -amd64
+CALL packages\KoreBuild\build\dotnetsdk upgrade -x64
+CALL packages\KoreBuild\build\dotnetsdk install default -runtime CoreCLR -x64
 CALL packages\KoreBuild\build\dotnetsdk install default -x86
 CALL packages\KoreBuild\build\dotnetsdk install default -runtime CoreCLR -x86
 
