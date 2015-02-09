440336d0fda57627133b80880ea881e2dff25f60
David Driscoll <david.driscoll@gmail.com>
2015-02-09 08:13:14 -0500

    Added type info Extensions/TypeInfoExtensions.cs

---
+++ src/Blacklite.Framework.Shared.Reflection/Extensions/TypeInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace System.Reflection
+{
+    public static class TypeInfoExtensions
+    {
+        public static PropertyInfo FindDeclaredProperty(this TypeInfo typeInfo, string name)
+        {
+            while (typeInfo != null)
+            {
+                var property = typeInfo.GetDeclaredProperty(name);
+                if (property != null)
+                    return property;
+
+                typeInfo = typeInfo?.BaseType?.GetTypeInfo();
+            }
+
+            return null;
+        }
+    }
+}
