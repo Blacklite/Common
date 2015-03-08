3c472296ff763877f4c8283eff04cd65c570ff52
David Driscoll <david.driscoll@gmail.com>
2015-03-08 19:36:30 -0400

    Updated for beta3

--- src/Blacklite.Framework.Interfaces/IServicesContext.cs
+++ src/Blacklite.Framework.Interfaces/IServicesContext.cs
@@ -1,13 +1,7 @@
-﻿#if ASPNET50 || ASPNETCORE50
-using Microsoft.Framework.Runtime;
-#endif
-using System;
+﻿using System;
 
 namespace Blacklite.Framework
 {
-#if ASPNET50 || ASPNETCORE50
-    [AssemblyNeutral]
-#endif
     public interface IServicesContext
     {
         IServiceProvider ServiceProvider { get; }
--- src/Blacklite.Framework.Interfaces/NotNullAttribute.cs
+++ src/Blacklite.Framework.Interfaces/NotNullAttribute.cs
@@ -1,13 +1,7 @@
-﻿#if ASPNET50 || ASPNETCORE50
-using Microsoft.Framework.Runtime;
-#endif
-using System;
+﻿using System;
 
 namespace Blacklite.Framework
 {
-#if ASPNET50 || ASPNETCORE50
-    [AssemblyNeutral]
-#endif
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
     public sealed class NotNullAttribute : Attribute
     {
